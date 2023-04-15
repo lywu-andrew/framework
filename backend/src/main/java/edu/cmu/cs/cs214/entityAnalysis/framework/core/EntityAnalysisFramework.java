9079391c4d286a236c77b53d63b49a32c8996510
Sunny Liang <sunnyliang6@gmail.com>
2023-04-15 19:44:45 -0400

    Created uploadData and getAnalyzedVisualization methods in the EntityAnalysisFramework interface

--- backend/src/main/java/edu/cmu/cs/cs214/entityAnalysis/framework/core/EntityAnalysisFramework.java
+++ backend/src/main/java/edu/cmu/cs/cs214/entityAnalysis/framework/core/EntityAnalysisFramework.java
@@ -1,5 +1,22 @@
 package main.java.edu.cmu.cs.cs214.entityAnalysis.framework.core;
 
-public class EntityAnalysisFramework {
-    
+public interface EntityAnalysisFramework {
+
+    /**
+     * Upload all of the files in the given local directory path.
+     * 
+     * Note that this directory should contain only files that match the type
+     * handled by the registered data plugin.
+     * 
+     * @param string The path to the local directory containing the data
+     */
+    void uploadData(String directoryPath);
+
+    /**
+     * Conduct the entity-sentiment analysis and get the visualization.
+     * 
+     * @return An HTML string of the generated visualization
+     */
+    String getAnalyzedVisualization();
+
 }
--- backend/src/main/java/edu/cmu/cs/cs214/entityAnalysis/framework/core/EntityAnalysisFrameworkImpl.java
+++ backend/src/main/java/edu/cmu/cs/cs214/entityAnalysis/framework/core/EntityAnalysisFrameworkImpl.java
@@ -1,5 +1,17 @@
 package main.java.edu.cmu.cs.cs214.entityAnalysis.framework.core;
 
-public class EntityAnalysisFrameworkImpl {
+public class EntityAnalysisFrameworkImpl implements EntityAnalysisFramework {
     
+    /**
+     * Source: https://www.tutorialspoint.com/how-to-read-data-from-all-files-in-a-directory-using-java
+     */
+    @Override
+    public void uploadData(String directoryPath) {
+        
+    }
+
+    @Override
+    public String getAnalyzedVisualization() {
+        return null;
+    }
 }
