6230c92b5afa2614e7b59a42a2d6dd59ff84d444
Sunny Liang <sunnyliang6@gmail.com>
2023-04-15 21:25:34 -0400

    Added starter code to App.java

--- backend/src/main/java/edu/cmu/cs214/hw6/App.java
+++ backend/src/main/java/edu/cmu/cs214/hw6/App.java
@@ -1,5 +1,7 @@
 package edu.cmu.cs214.hw6;
 
+import java.io.IOException;
+
 import fi.iki.elonen.NanoHTTPD;
 
 /**
@@ -10,5 +12,18 @@ public class App extends NanoHTTPD {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+
+    public App() throws IOException {
+        super(8080);
+
+        start(NanoHTTPD.SOCKET_READ_TIMEOUT, false);
+        System.out.println("\nRunning!\n");
+    }
+
+    @Override
+    public Response serve(IHTTPSession session) {
+        Response response = newFixedLengthResponse("");
+        return response;
     }
 }
