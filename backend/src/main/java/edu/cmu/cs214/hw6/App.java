03ba185b17ad98eb3e69e8ecac37b36c80eef1af
Sunny Liang <sunnyliang6@gmail.com>
2023-04-15 21:16:39 -0400

    Added NanoHTTPD to project

--- backend/src/main/java/edu/cmu/cs214/hw6/App.java
+++ backend/src/main/java/edu/cmu/cs214/hw6/App.java
@@ -1,11 +1,12 @@
 package edu.cmu.cs214.hw6;
 
+import fi.iki.elonen.NanoHTTPD;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
+public class App extends NanoHTTPD {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
