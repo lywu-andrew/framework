dfc57341bf20531f0d69016d64319ee289c72d3a
Sunny Liang <sunnyliang6@gmail.com>
2023-04-18 16:37:39 -0400

    Switched to use Apache PDFBox to parse PDF

--- backend/src/main/java/edu/cmu/cs214/hw6/plugin/PdfPlugin.java
+++ backend/src/main/java/edu/cmu/cs214/hw6/plugin/PdfPlugin.java
@@ -1,15 +1,15 @@
 package edu.cmu.cs214.hw6.plugin;
 
+import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 
 import edu.cmu.cs214.hw6.framework.core.DataPlugin;
 import edu.cmu.cs214.hw6.framework.core.ESAFramework;
 
-import com.snowtide.PDF;
-import com.snowtide.pdf.Document;
-import com.snowtide.pdf.OutputTarget;
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
 
 public class PdfPlugin implements DataPlugin {
     private final String PLUGIN_NAME = "pdf";
@@ -26,18 +26,25 @@ public class PdfPlugin implements DataPlugin {
 
     /**
      * Convert the text in the PDF at the given path to a String.
+     * https://www.tutorialspoint.com/how-to-read-data-from-pdf-file-and-display-on-console-in-java
      * @throws IOException
      */
     @Override
     public String convertToString(Path filePath) throws IOException {
-        String pdfFilePath = filePath.toString();
- 
-        Document pdf = PDF.open(pdfFilePath);
-        StringBuilder text = new StringBuilder(1024);
-        pdf.pipe(new OutputTarget(text));
-        pdf.close();
+        // Loading an existing document
+        File file = filePath.toFile();
+        PDDocument document = Loader.loadPDF(file);
+
+        // Instantiate PDFTextStripper class
+        PDFTextStripper pdfStripper = new PDFTextStripper();
+
+        // Retrieving text from PDF document
+        String text = pdfStripper.getText(document);
+
+        // Closing the document
+        document.close();
         
-        return text.toString();
+        return text;
     }
 
     /**
