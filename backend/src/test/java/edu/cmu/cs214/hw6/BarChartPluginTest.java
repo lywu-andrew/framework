749b0f70181f2e87de6daa8ff26d6234ccb46099
lywu-andrew <lywu@andrew.cmu.edu>
2023-04-18 22:13:39 -0400

    started visualization plugin testing

---
+++ backend/src/test/java/edu/cmu/cs214/hw6/BarChartPluginTest.java
@@ -0,0 +1,23 @@
+package edu.cmu.cs214.hw6;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.cmu.cs214.hw6.plugin.visualization.BarChartPlugin;
+
+import static org.junit.Assert.*;
+
+public class BarChartPluginTest {
+
+    private BarChartPlugin plugin;
+
+    @Before
+    public void setUp() {
+        this.plugin = new BarChartPlugin();
+    }
+
+    @Test
+    public void testBarChartVisualization() {
+        
+    }
+}
